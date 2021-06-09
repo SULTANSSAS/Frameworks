354cbdb8e089c8c8a76eccdeb4c893b173560964
13653 <13653@192.168.0.18>
2021-06-09 15:12:36 -0400

    Move to Local Repository

---
+++ POM/src/com/webappsecurity/zero/Pages/AccountSummary.java
@@ -0,0 +1,20 @@
+package com.webappsecurity.zero.Pages;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+
+public class AccountSummary {
+	@FindBy(linkText = "Transfer Funds")
+	private WebElement transferFunds;
+	public AccountSummary (WebDriver driver) {
+		PageFactory.initElements(driver,this);
+	}
+	public void click111()
+	{
+		transferFunds.click();
+	}
+	
+
+}
